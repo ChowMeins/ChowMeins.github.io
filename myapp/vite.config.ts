0381649d15713ad63999c706525410b55b16fd4e
ChowMeins <jasonchau4627@gmail.com>
2024-09-25 18:44:51 -0500

    Adjusting config files

--- myapp/vite.config.ts
+++ myapp/vite.config.ts
@@ -3,8 +3,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	base: './',
-	build: {
-		outDir: 'docs'  // Change the output directory to docs
-	  }
+	base: '/myapp/',
 });
