76ce82c3c7fe002a8a40bf8d82528b4eb56e4b3e
ChowMeins <jasonchau4627@gmail.com>
2024-09-26 02:13:15 -0500

    Added Roboto Font

--- myapp/src/components/navbar.svelte
+++ myapp/src/components/navbar.svelte
@@ -1,6 +1,14 @@
 <div class='h-fit bg-[#0d1b2a] shadow-xl p-4'>
-    <ul class='text-3xl text-white flex gap-4'>
-        <li> <a href='/'> Home </a> </li>
-        <li> <a href='/tools'> Tools </a> </li>
+    <ul class='roboto-bold text-2xl text-white flex gap-4'>
+        <li> <a href='/'> HOME </a> </li>
+        <li> <a href='/tools'> TOOLS </a> </li>
     </ul>
 </div>
+
+<style>
+    .roboto-bold {
+        font-family: "Roboto", system-ui;
+        font-weight: 700;
+        font-style: normal;
+    }
+</style>
--- myapp/src/routes/tools/calculateTips/+page.svelte
+++ myapp/src/routes/tools/calculateTips/+page.svelte
@@ -16,8 +16,8 @@
 <div class='w-full h-[100%] bg-[#1b263b]'>
     <ul class='w-full bg-[#1b263b] text-white flex flex-wrap'>
         {#each days_of_week as day, i}
-            <h1 class='w-full bg-[#415a77]'> {day} </h1>
-            <div class='w-[90%] py-4 flex'>
+            <h1 class='w-full pl-4 py-1 bg-[#415a77]'> {day} </h1>
+            <div class='w-[90%] pt-1 pb-4 flex'>
                 <div class='w-[45%] mr-auto'>
                     <p class='ml-3'> Cash </p> 
                     <input type="number" bind:value={weeklyCash[i]} on:input={calcSum} placeholder="Enter the total here" class="p-1 text-black border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
