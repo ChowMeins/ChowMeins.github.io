e533ab582a1090588b5807c3763f8b6acbff7a74
ChowMeins <jasonchau4627@gmail.com>
2024-10-01 06:20:08 -0500

    env file added with keys

--- myapp/src/firebaseConfig.js
+++ myapp/src/firebaseConfig.js
@@ -1,16 +1,28 @@
 import { initializeApp } from "firebase/app";
 import { getStorage, ref } from "firebase/storage";
+import {
+  PUBLIC_FIREBASE_API_KEY,
+  PUBLIC_FIREBASE_AUTH_DOMAIN,
+  PUBLIC_FIREBASE_DATABASE_URL,
+  PUBLIC_FIREBASE_PROJECT_ID,
+  PUBLIC_FIREBASE_STORAGE_BUCKET,
+  PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  PUBLIC_FIREBASE_APP_ID,
+  PUBLIC_FIREBASE_MEASUREMENT_ID
+} from "$env/static/public";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBhgiEvBBm_Gl5RMdkxAiCufH89RJ-fLIc",
-    authDomain: "jason-s-personal-website.firebaseapp.com",
-    projectId: "jason-s-personal-website",
-    storageBucket: "jason-s-personal-website.appspot.com",
-    messagingSenderId: "56309379579",
-    appId: "1:56309379579:web:acbb87ee4c8f9aaccf9b5e",
-    measurementId: "G-2CBD7KEEVQ"
-  };
+  apiKey: PUBLIC_FIREBASE_API_KEY,
+  authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: PUBLIC_FIREBASE_APP_ID,
+  measurementId: PUBLIC_FIREBASE_MEASUREMENT_ID
+};
+console.log();
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
