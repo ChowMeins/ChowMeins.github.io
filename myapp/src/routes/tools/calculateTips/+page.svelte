3304ff35a4705eba10a8f9ec322922dc87a9e54b
ChowMeins <jasonchau4627@gmail.com>
2024-09-30 22:57:30 -0500

    tips calculator logic fixed, error messages added

--- myapp/src/routes/tools/calculateTips/+page.svelte
+++ myapp/src/routes/tools/calculateTips/+page.svelte
@@ -1,14 +1,58 @@
 <script lang='ts'>
+    import { onMount } from "svelte";
 
-    let weeklyCash: (number | '')[] = Array(7).fill('');
-    let weeklyCredit: (number | '')[] = Array(7).fill('');
+    let weeklyCash: (string | '')[] = Array(7).fill('');
+    let weeklyCredit: (string | '')[] = Array(7).fill('');
+    let cashInput: Array<[(HTMLElement | null), boolean]> = Array(7).fill([null, true]);
+    let creditInput: Array<[(HTMLElement | null), boolean]> = Array(7).fill([null, true]);
     let days_of_week: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    let sum: number = 0;
-    function calcSum() {
-        sum = 0;
-        for(let i = 0; i < weeklyCash.length; ++i) {
-            sum += (weeklyCash[i] == '' ? 0 : Number(weeklyCash[i])) + (weeklyCredit[i] == '' ? 0 : Number(weeklyCredit[i]));
+    let sum: string = "";
+
+    onMount(() => {
+        for(let i = 0; i < cashInput.length; ++i) {
+            cashInput[i] = [document.getElementById('cash-' + i), true];
+            creditInput[i] = [document.getElementById('credit-' + i), true];
         }
+    });
+
+    function validateAmount(amount: string, index: number, type: string): boolean {
+        let amountParts: string[] = amount.toString().split(".");
+
+        if (amountParts.length === 1 || (amountParts.length === 2 && amountParts[1].length <= 2)) {
+            if(type === "cash") {
+                cashInput[index][1] = true;
+            }
+            else if (type === "credit") { 
+                creditInput[index][1] = true;
+            }
+            return true;
+        }
+        else if (amountParts.length === 2 && amountParts[1].length > 2) {
+            if(type === "cash") {
+                cashInput[index][1] = false;
+            }
+            else if (type === "credit") { 
+                creditInput[index][1] = false;
+            }
+            console.log("Exited");
+            return false;
+        }
+        return true;
+    }
+
+    function calcSum(): void {
+        let temp: number = 0;
+        sum = "";
+        for(let i = 0; i < weeklyCash.length; ++i) {
+            temp += (weeklyCash[i] == '' ? 0 : Number(weeklyCash[i])) + (weeklyCredit[i] == '' ? 0 : Number(weeklyCredit[i]));
+        }
+        temp = Math.trunc(temp * 100) / 100
+        let tempToString: string[] = temp.toString().split(".");
+        if (tempToString.length === 2 && tempToString[1].length === 1) {
+            tempToString[1] += "0"
+        }
+
+        sum = "$" + tempToString.join(".");
     }
 
 </script>
@@ -19,24 +63,54 @@
             <h1 class='w-full pl-4 py-1 bg-[#415a77]'> {day} </h1>
             <div class='w-[90%] pt-1 pb-4 flex'>
                 <div class='w-[45%] mr-auto'>
-                    <p class='ml-3'> Cash </p> 
-                    <input type="number" bind:value={weeklyCash[i]} on:input={calcSum} placeholder="Enter the total here" class="p-1 text-black border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                    <p class='ml-3'> Cash </p>  
+                    <input id="cash-{i}" type="number" pattern="[0-9]*" inputmode="decimal" placeholder="Enter the total here" bind:value={weeklyCash[i]} 
+                        on:input={() => {
+                        validateAmount(weeklyCash[i], i, "cash")
+                        calcSum()}} />
+                    {#if cashInput[i][1] === false}
+                        <p class='warning'> ⚠ Warning: Input is invalid. Please use digits up to 2 decimal places. Calculation may be incorrect</p>
+                    {/if}
                 </div>
                 <div class='w-[45%] ml-auto '>
                     <p class='ml-3'> Credit </p>
-                    <input type="number" bind:value={weeklyCredit[i]} on:input={calcSum} placeholder="Enter the total here" class="p-1 text-black border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                    <input id="credit-{i}" type="number" pattern="[0-9]*" inputmode="decimal" placeholder="Enter the total here"  bind:value={weeklyCredit[i]} 
+                    on:input={() => {
+                        validateAmount(weeklyCredit[i], i, "credit")
+                        calcSum()}} />
+                    {#if creditInput[i][1] === false}
+                        <p class='warning'> ⚠ Warning: Invalid Input. Please use digits up to 2 decimal places. Calculation may be incorrect</p>
+                    {/if}
                 </div>
             </div>
         {/each}
     </ul>
-    <p class='bg-[#0d1b2a] text-white pl-4'> Total: ${sum} </p>
+    <p class='bg-[#0d1b2a] text-white pl-4'> Total: {sum} </p>
 </div>
 
 <style>
     input {
         width: 100%;
-        margin-left: 0.75rem;
-        margin-right: 0.75rem;
+        padding: 0.25rem;
+        border-radius: 5px;
+        border: none;
+        margin: 0 0.75rem 0.5rem 0.75rem;
+        color: black;
     }
+    input:focus-visible {
+        outline: 3.5px solid #1296ee;
+    }
+    .warning {
+        width: 100%;
+        margin-left: 0.75rem;
+        padding: 0 0.5rem 0 0.25rem;
+        font-size: calc(0.55vw + 0.55vh);
+        text-align: center;
+        background-color: #e58080;
+        border: 2.5px solid #d65050;
+        border-radius: 3px;
+        color: white;        
+    }
+
 
 </style>
