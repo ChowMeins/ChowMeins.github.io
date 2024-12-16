774ca4763106fcbf5910260a02d47c591831f28d
ChowMeins <jasonchau4627@gmail.com>
2024-12-16 07:34:45 -0600

    Adjusted formatting of images for responsive design

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -24,7 +24,7 @@
         <div class='profile'>
             <div class='w-full h-full flex row flex-wrap'>
                 <p class='w-full h-fit py-4 text-white font-semibold text-5xl text-center drop-shadow-md'> Hello, I'm Jason. 👋</p>
-                <img src='/hamster-camera.gif' class='profile-pic border-1 border-[#FFFFFF] rounded-full drop-shadow-xl' alt='My IRL Pic'/>
+                <img src='/hamster-camera.gif' class='profile-pic border-3 border-[#FFFFFF] rounded-full drop-shadow-xl' alt='My IRL Pic'/>
             </div>
         </div>
         <!-- Right side of home page -->
@@ -58,18 +58,18 @@
                         <li> <a href='http://pkmn-tracker.vercel.app/' target="_blank"><img src={techStack.get('internet')} alt="GitHub" class='w-[32px] h-[32px]'></a></li>
                     </ul>
                 </h1>
-                <img src={images[0][0] ? images[0][2] : images[0][1]} alt='PokeTracker Demo' class='w-fit max-xl:h-[320px]' on:mouseenter={() => images[0][0] = true} on:mouseleave={() => images[0][0] = false}/>
+                <img src={images[0][0] ? images[0][2] : images[0][1]} alt='PokeTracker Demo' class='w-fit h-auto mx-auto' on:mouseenter={() => images[0][0] = true} on:mouseleave={() => images[0][0] = false}/>
                 <p class='text-[18px] font-medium'> 
                     A web application for Pokémon enthusiasts to track their shiny hunting progress. 
                     Built with SvelteKit, TypeScript, Tailwind, and Firebase, it features real-time data management, optimized Pokémon image loading via PokeAPI, EV tracking, and secure user authentication. 
                     With a responsive designed interface ensure a seamless experience for all platforms. 
                 </p>
                 <ul class='flex gap-3 mt-auto ml-auto'> 
-                    <img class='w-[32px] h-[32px] max-sm:w-[48px] max-sm:h-[48px]' src={techStack.get('svelte')} alt='Svelte' title='Svelte'/>
-                    <img class='w-[32px] h-[32px] max-sm:w-[48px] max-sm:h-[48px]' src={techStack.get('typescript')} alt='TypeScript' title='TypeScript'/>
-                    <img class='w-[32px] h-[32px] max-sm:w-[48px] max-sm:h-[48px]' src={techStack.get('tailwind')} alt='Tailwind' title='Tailwind'/>
-                    <img class='w-[32px] h-[32px] max-sm:w-[48px] max-sm:h-[48px]' src={techStack.get('firebase')} alt='Firebase' title='Firebase'/>
-                    <img class='w-[48px] h-fit my-auto' src='pokeapi.png' alt='PokeAPI' title='PokeAPI'/>
+                    <img class='w-[32px] h-[32px] sm:w-[40px] sm:h-[40px]' src={techStack.get('svelte')} alt='Svelte' title='Svelte'/>
+                    <img class='w-[32px] h-[32px] sm:w-[40px] sm:h-[40px]' src={techStack.get('typescript')} alt='TypeScript' title='TypeScript'/>
+                    <img class='w-[32px] h-[32px] sm:w-[40px] sm:h-[40px]' src={techStack.get('tailwind')} alt='Tailwind' title='Tailwind'/>
+                    <img class='w-[32px] h-[32px] sm:w-[40px] sm:h-[40px]' src={techStack.get('firebase')} alt='Firebase' title='Firebase'/>
+                    <img class='w-[60px] h-[25px] my-auto' src='pokeapi.png' alt='PokeAPI' title='PokeAPI'/>
                 </ul>
             </section>
             <!-- Wizard101 Quiz Solver -->
@@ -80,7 +80,7 @@
                         <li> <a href='https://github.com/ChowMeins/Wizard101-Quiz-Solver' target="_blank"><img src={techStack.get('github_white')} alt="GitHub" class='w-[32px] h-[32px]'></a></li>
                     </ul>
                 </h1>
-                <img src={images[1][0] ? images[1][2] : images[1][1]} alt='PokeTracker Demo' class='w-fit max-xl:h-[320px]' on:mouseenter={() => images[1][0] = true} on:mouseleave={() => images[1][0] = false}/>
+                <img src={images[1][0] ? images[1][2] : images[1][1]} alt='PokeTracker Demo' class='w-full h-fit mx-auto' on:mouseenter={() => images[1][0] = true} on:mouseleave={() => images[1][0] = false}/>
                 <p class='text-[18px] font-medium'> 
                     A Python script that allows users to automatically completed trivia for crowns (in-game currency). 
                     The Selenium package is used to automate web browser interactions to both click and navigate through the questions.
@@ -89,10 +89,10 @@
                     
                 </p>
                 <ul class='flex gap-3 mt-auto ml-auto'> 
-                    <img class='w-[32px] h-[32px] max-sm:w-[48px] max-sm:h-[48px]' src={techStack.get('python')} alt='Python' title='Python'/>
-                    <img class='w-[32px] h-[32px] max-sm:w-[48px] max-sm:h-[48px]' src={techStack.get('raspberryPi')} alt='Raspberry Pi' title='Raspberry Pi'/>
-                    <img class='w-[32px] h-[32px] max-sm:w-[48px] max-sm:h-[48px]' src={techStack.get('bash')} alt='Bash' title='Bash'/>
-                    <img class='w-[32px] h-[32px] max-sm:w-[48px] max-sm:h-[48px]' src={techStack.get('openAI')} alt='OpenAI' title='OpenAI'/>
+                    <img class='w-[32px] h-[32px] sm:w-[40px] sm:h-[40px]' src={techStack.get('python')} alt='Python' title='Python'/>
+                    <img class='w-[32px] h-[32px] sm:w-[40px] sm:h-[40px]' src={techStack.get('raspberryPi')} alt='Raspberry Pi' title='Raspberry Pi'/>
+                    <img class='w-[32px] h-[32px] sm:w-[40px] sm:h-[40px]' src={techStack.get('bash')} alt='Bash' title='Bash'/>
+                    <img class='w-[32px] h-[32px] sm:w-[40px] sm:h-[40px]' src={techStack.get('openAI')} alt='OpenAI' title='OpenAI'/>
                 </ul>
             </section>
             <!-- Musi To YouTube Playlist Converter -->
@@ -103,7 +103,7 @@
                         <li> <a href='https://github.com/ChowMeins/Musi-to-YouTube-Playlist' target="_blank"><img src={techStack.get('github_white')} alt="GitHub" class='w-[32px] h-[32px]'></a></li>
                     </ul>
                 </h1>
-                <img src='demos/MusiToYouTube.png' alt='Musi to YouTube playlist converter' class='w-fit max-xl:h-[320px]' on:mouseenter={() => images[0][0] = true} on:mouseleave={() => images[0][0] = false}/>
+                <img src='demos/MusiToYouTube.png' alt='Musi to YouTube playlist converter' class='w-full h-full mx-auto' on:mouseenter={() => images[0][0] = true} on:mouseleave={() => images[0][0] = false}/>
                 <p class='text-[18px] font-medium'> 
                     A Python script that converts playlists from the mobile app Musi to YouTube.
                     Utilizes the following packages: Selenium to execute a headless chrome web driver, Beautiful Soup to scrape the YouTube links from the Musi URL,
@@ -121,16 +121,16 @@
                         <li> <a href='https://github.com/ChowMeins/Pokemon-Shiny-Hunter-Bot' target="_blank"><img src={techStack.get('github_white')} alt="GitHub" class='w-[32px] h-[32px]'></a></li>
                     </ul>
                 </h1>
-                <img src={images[2][0] ? images[2][2] : images[2][1]} alt='Pokemon Shiny Hunting Bot Demo' class='w-fit max-xl:h-[320px]' on:mouseenter={() => images[2][0] = true} on:mouseleave={() => images[2][0] = false}/>
+                <img src={images[2][0] ? images[2][2] : images[2][1]} alt='Pokemon Shiny Hunting Bot Demo' class='w-full h-fit mx-auto' on:mouseenter={() => images[2][0] = true} on:mouseleave={() => images[2][0] = false}/>
                 <p class='text-[18px] font-medium'> 
                     A Python script that sends an Arduino commands in to operate a Nintendo 3DS to shiny hunt Pokemon.
                     Detection methods included image comparison with OpenCV/NumPy and photoresistor to detect light intensity, 
                     while servo motors press buttons to automate dialogue interactions and encounters.
                 </p>
                 <ul class='flex gap-3 mt-auto ml-auto'> 
-                    <img class='w-[32px] h-[32px] max-sm:w-[48px] max-sm:h-[48px]' src={techStack.get('python')} alt='Python' title='Python'/>
-                    <img class='w-[32px] h-[32px] max-sm:w-[48px] max-sm:h-[48px]' src={techStack.get('arduino')} alt='Arduino' title='Arduino'/>
-                    <img class='w-[32px] h-[32px] max-sm:w-[48px] max-sm:h-[48px]' src={techStack.get('c++')} alt='C++' title='C++'/>
+                    <img class='w-[32px] h-[32px] sm:w-[40px] sm:h-[40px]' src={techStack.get('python')} alt='Python' title='Python'/>
+                    <img class='w-[32px] h-[32px] sm:w-[40px] sm:h-[40px]' src={techStack.get('arduino')} alt='Arduino' title='Arduino'/>
+                    <img class='w-[32px] h-[32px] sm:w-[40px] sm:h-[40px]' src={techStack.get('c++')} alt='C++' title='C++'/>
                 </ul>
             </section>
             <!-- Comet Dash -->
@@ -141,15 +141,15 @@
                         <li> <a href='https://github.com/ZaneLeBlanc/Team16Project' target="_blank"><img src={techStack.get('github_white')} alt="GitHub" class='w-[32px] h-[32px]'></a></li>
                     </ul>
                 </h1>
-                <img src={images[3][0] ? images[3][2] : images[3][1]} alt='PokeTracker Demo' class='w-fit max-xl:h-[320px]' on:mouseenter={() => images[3][0] = true} on:mouseleave={() => images[3][0] = false}/>
+                <img src={images[3][0] ? images[3][2] : images[3][1]} alt='PokeTracker Demo' class='w-full h-fit mx-auto' on:mouseenter={() => images[3][0] = true} on:mouseleave={() => images[3][0] = false}/>
                 <p class='text-[18px] font-medium'> 
                     A game made in Unity in the style of a infinite-runner. Implemented infinite-level generation, including dynamic road and terrain creation,
                     a custom jump function with realistic gravitational physics to enhance the car's movement and gameplay mechanics.
                     Optimized procedural generation for efficient performance, memory management, and smooth transitions between generated segments.
                 </p>
                 <ul class='flex gap-3 mt-auto ml-auto'> 
-                    <img class='w-[32px] h-[32px] max-sm:w-[48px] max-sm:h-[48px]' src={techStack.get('unity')} alt='Unity' title='Unity'/>
-                    <img class='w-[32px] h-[32px] max-sm:w-[48px] max-sm:h-[48px]' src={techStack.get('c_sharp')} alt='C#' title='C#'/>
+                    <img class='w-[32px] h-[32px] sm:w-[40px] sm:h-[40px]' src={techStack.get('unity')} alt='Unity' title='Unity'/>
+                    <img class='w-[32px] h-[32px] sm:w-[40px] sm:h-[40px]' src={techStack.get('c_sharp')} alt='C#' title='C#'/>
                 </ul>
             </section>
             <!-- Aggregate Shopping Cart -->
@@ -160,7 +160,7 @@
                         <li> <a href='https://github.com/ChosenQuill/uARexpert' target="_blank"><img src={techStack.get('github_white')} alt="GitHub" class='w-[32px] h-[32px]'></a></li>
                     </ul>
                 </h1>
-                <iframe class='w-full h-[320px]' src="https://www.youtube.com/embed/_V5PumrW9W8?si=E_mkklOrUBclv6du" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <iframe class='w-full h-[320px] mx-auto' src="https://www.youtube.com/embed/_V5PumrW9W8?si=E_mkklOrUBclv6du" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                 <p class='text-[18px] font-medium'> 
                     Sponsored by uARExpert, the Aggregate Shopping Cart provides a seamless virtual shopping cart between users and experts alike using React, PostgreSQL, and Chakra UI. 
                     The shopping cart will aggregate and source products/services, configuring it to provide multiple rounds of review before the products are sourced. 
@@ -168,8 +168,8 @@
                     the ability to manage project pages, incorporation of shopping carts for each project page, and integration within a database.
                 </p>
                 <ul class='flex gap-3 mt-auto ml-auto'> 
-                    <img class='w-[32px] h-[32px] max-sm:w-[48px] max-sm:h-[48px]' src={techStack.get('react')} alt='React' title='React'/>
-                    <img class='w-[32px] h-[32px] max-sm:w-[48px] max-sm:h-[48px]' src={techStack.get('postgresql')} alt='PostgreSQL' title='PostgreSQL'/>
+                    <img class='w-[32px] h-[32px] sm:w-[40px] sm:h-[40px]' src={techStack.get('react')} alt='React' title='React'/>
+                    <img class='w-[32px] h-[32px] sm:w-[40px] sm:h-[40px]' src={techStack.get('postgresql')} alt='PostgreSQL' title='PostgreSQL'/>
                 </ul>
             </section>
             <!-- Personal Portfolio Website -->
@@ -181,14 +181,14 @@
                         <li> <a href='https://chowmeins.github.io/'><img src={techStack.get('internet')} alt="Website" class='w-[32px] h-[32px]'></a></li>
                     </ul>
                 </h1>
-                <img src='demos/PersonalPortfolio.png' alt='PokeTracker Demo' class='w-fit max-xl:h-[320px]' on:mouseenter={() => images[2][0] = true} on:mouseleave={() => images[2][0] = false}/>
+                <img src='demos/PersonalPortfolio.png' alt='PokeTracker Demo' class='w-full h-fit mx-auto' on:mouseenter={() => images[2][0] = true} on:mouseleave={() => images[2][0] = false}/>
                 <p class='text-[18px] font-medium'> 
                     My personal website showcasing me! Includes things such as my portfolio, projects, and tools for my personal use that are public!
                 </p>
                 <ul class='flex gap-3 mt-auto ml-auto'> 
-                    <img class='w-[32px] h-[32px] max-sm:w-[48px] max-sm:h-[48px]' src={techStack.get('svelte')} alt='Svelte' title='Svelte'/>
-                    <img class='w-[32px] h-[32px] max-sm:w-[48px] max-sm:h-[48px]' src={techStack.get('tailwind')} alt='Tailwind' title='Tailwind'/>
-                    <img class='w-[32px] h-[32px] max-sm:w-[48px] max-sm:h-[48px]' src={techStack.get('supabase')} alt='Supabase' title='Supabase'/>
+                    <img class='w-[32px] h-[32px] sm:w-[40px] sm:h-[40px]' src={techStack.get('svelte')} alt='Svelte' title='Svelte'/>
+                    <img class='w-[32px] h-[32px] sm:w-[40px] sm:h-[40px]' src={techStack.get('tailwind')} alt='Tailwind' title='Tailwind'/>
+                    <img class='w-[32px] h-[32px] sm:w-[40px] sm:h-[40px]' src={techStack.get('supabase')} alt='Supabase' title='Supabase'/>
                 </ul>
             </section>
             
